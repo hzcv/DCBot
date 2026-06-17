993b70ba623efc3fc70705677803c66b9fdec36a
Hrss <tackyverse5952@gmail.com>
2026-06-18 01:42:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 # ==========================
 # BOT CONFIG
 # ==========================
-BOT_TOKEN = MTUxNjg5NjEzMjg1MTE3MTUwOA.GlYN3L.0_NWwZeZTCIRmP_45wGAocLwQS3JYxBxCZOmwU
+BOT_TOKEN = "MTUxNjg5NjEzMjg1MTE3MTUwOA.GlYN3L.0_NWwZeZTCIRmP_45wGAocLwQS3JYxBxCZOmwU"
 
 # Your Discord User ID
 ADMIN_ID = 1325100388944183379
